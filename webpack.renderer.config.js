fd61fb79b2a98656f76d7afe8e145af79ad8cc34
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-08-29 19:11:40 +0200

    Added mui, configured alias paths

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
+
+const createAliasPath = (p) => path.join(__dirname, p);
 
 rules.push({
   test: /\.css$/,
@@ -12,6 +15,11 @@ module.exports = {
   },
   plugins: plugins,
   resolve: {
+    alias: {
+      '@mui/styled-engine': createAliasPath('node_modules/@mui/styled-engine-sc'),
+      '@hooks': createAliasPath('src/hooks'),
+      '@pages': createAliasPath('src/pages'),
+    },
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
 };
