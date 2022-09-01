467dcd32790bfbafc60320026f954e83e768bd9e
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-01 20:37:35 +0200

    Added styles for page mount point for appropriate scrolling behavior

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -3,12 +3,14 @@ import styled from "styled-components";
 export const AppContainer = styled.div`
   display: flex;
   min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.white.main};
 `;
 
 export const PageMountPoint = styled.main`
   flex: 1;
-
   background-color: ${({ theme }) => theme.colors.white.main};
-
-  padding: 24px;
+  position: relative;
+  height: 100%;
+  max-height: 100vh;
+  overflow: auto;
 `;
