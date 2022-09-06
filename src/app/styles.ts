ed4a2ed6e4e5074e1c3a03bc2fbd391597f7da52
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-06 15:22:37 +0200

    Fixed app layout

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -3,17 +3,16 @@ import styled from "styled-components";
 export const AppContainer = styled.div`
   display: flex;
   min-height: 100vh;
-  background-color: ${({ theme }) => theme.colors.white.main};
 `;
 
 export const PageMountPoint = styled.main`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors.white.main};
   position: relative;
   height: 100%;
   max-height: 100vh;
   min-height: 100vh;
   overflow: auto;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: 1fr;
 `;
