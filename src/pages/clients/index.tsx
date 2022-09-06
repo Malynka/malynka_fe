aff22298943e44bbabfe23c70a48fe2a0eaaf405
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-06 15:23:37 +0200

    Implemented clients page

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,10 +1,43 @@
 import React, { FunctionComponent } from "react";
+import { GridColumns, GridRenderCellParams, ukUA } from '@mui/x-data-grid';
+import { IconButton } from "@mui/material";
+import EditIcon from '@mui/icons-material/EditRounded';
+import DeleteIcon from '@mui/icons-material/DeleteRounded';
 import AddIcon from '@mui/icons-material/PersonAddAlt1Rounded';
-import { RoundedButton } from "@molecules"; 
+import { RoundedButton } from "@molecules";
 import { Header } from "@organisms";
 import { useDocumentTitle } from "@hooks";
 import { IPageProps } from "../types";
-import { ClientsContainer } from "./styles";
+import { ClientsContainer, DataGrid } from "./styles";
+
+const col: GridColumns = [
+  {
+    field: 'name',
+    headerName: "Ім'я",
+    flex: 1
+  },
+  {
+    field: 'note',
+    headerName: 'Примітка',
+    flex: 1
+  },
+  {
+    field: 'actions',
+    headerName: 'Дії',
+    sortable: false,
+    hideable: false,
+    renderCell: (params: GridRenderCellParams) => (
+      <>
+        <IconButton onClick={() => console.log('Edit ID:', params.id)}>
+          <EditIcon />
+        </IconButton>
+        <IconButton onClick={() => console.log('Remove ID:', params.id)}>
+          <DeleteIcon />
+        </IconButton>
+      </>
+    )
+  }
+];
 
 const Clients: FunctionComponent<IPageProps> = ({ name }) => {
   useDocumentTitle(name);
@@ -22,7 +55,15 @@ const Clients: FunctionComponent<IPageProps> = ({ name }) => {
         }
       />
       <ClientsContainer>
-        Clients Page
+        <DataGrid
+          columns={col}
+          rows={new Array(30).fill(0).map((_, i) => ({
+            id: i,
+            name: 'Чернявка саня',
+            note: 'Примітка'
+          }))}
+          localeText={ukUA.components.MuiDataGrid.defaultProps.localeText}
+        />
       </ClientsContainer>
     </>
   );
--- src/pages/clients/styles.ts
+++ src/pages/clients/styles.ts
@@ -1,7 +1,29 @@
 import styled from "styled-components";
+import { Body } from "@typography";
+import { DataGrid as MuiDataGrid, gridClasses } from "@mui/x-data-grid";
 
 export const ClientsContainer = styled.div`
-  padding-left: 24px;
-  padding-right: 24px;
-  padding-bottom: 24px;
+`;
+
+export const DataGrid = styled(MuiDataGrid)`
+  &.${gridClasses.root} {
+    ${Body.getStyles()}
+
+    .${gridClasses.columnHeader} {
+      background-color: ${({ theme }) => theme.colors.black.light};
+      color: ${({ theme }) => theme.colors.white.main};
+
+      .${gridClasses.iconButtonContainer} svg, .${gridClasses.menuIconButton} svg {
+        color: ${({ theme }) => theme.colors.white.main};
+      }
+    }
+
+    .${gridClasses.row} {
+      background-color: ${({ theme }) => theme.colors.white.main};
+
+      &:nth-of-type(2n) {
+        background-color: ${({ theme }) => theme.colors.white.dark};
+      }
+    }
+  }
 `;
