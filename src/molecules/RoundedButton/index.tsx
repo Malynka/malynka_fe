5490c9d2dfe57b3f00f62da72429be854985a3f2
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-06 19:58:20 +0200

    Added additional props to RoundedButton molecule

--- src/molecules/RoundedButton/index.tsx
+++ src/molecules/RoundedButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactNode, MouseEventHandler } from 'react';
 import { Body } from '@typography';
 import { MuiButton } from './styles';
 
@@ -7,16 +7,26 @@ export interface IRoundedButtonProps {
   text: string;
   variant?: 'contained' | 'outlined' | 'text';
   color?: 'primary' | 'secondary' | 'error' | 'info' | 'inherit' | 'success' | 'warning';
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
 const RoundedButton: FunctionComponent<IRoundedButtonProps> = ({
   icon,
   text,
   variant,
-  color
+  color,
+  onClick,
+  disabled,
 }) => {
   return (
-    <MuiButton variant={variant} color={color} startIcon={icon}>
+    <MuiButton
+      variant={variant}
+      color={color}
+      startIcon={icon}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <Body>{ text }</Body>
     </MuiButton>
   );
