23936195b243504aae918cdf32087523c4808d1d
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-08 18:01:04 +0200

    Fixed bug with month in getFormattedDate util

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
-export const getSpacedDecimal = (num: number) => String(num).split('').reverse().reduce((acc, curr, index) => acc + curr + ((index + 1) % 3 === 0 ? ' ' : ''), '').split('').reverse().join('').trim();
+export const getSpacedDecimal = (num: number) => String(num).split('').reverse().reduce((acc, curr, index) => acc + curr + ((index + 1) % 3 === 0 ? '\xa0' : ''), '').split('').reverse().join('').trim();
 
 export const getFormattedDate = (timestamp: number) => {
   const date = new Date(timestamp);
 
   const day = date.getDate();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
 
   return`${day / 10 >= 1 ? day : '0' + day}.${month / 10 >= 1 ? month : '0' + month}.${date.getFullYear()}`;
 };
