9a0539f5a62e8200dc96d5d8b5344844064f1d31
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-10 16:03:11 +0200

    Implemented receivings page's logic

--- src/pages/receivings/index.tsx
+++ src/pages/receivings/index.tsx
@@ -1,25 +1,215 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect, Suspense } from 'react';
+import { CircularProgress } from '@mui/material';
+import dayjs, { Dayjs } from 'dayjs';
 import AddIcon from '@mui/icons-material/Add';
-import { RoundedButton, Table, TableHeader } from '@molecules';
-import { Header, ReceivingTableRow } from '@organisms';
+import AllWeightIcon from '@mui/icons-material/AllInboxRounded';
+import AllPriceIcon from '@mui/icons-material/AttachMoneyRounded';
+import { GridValidRowModel } from '@mui/x-data-grid';
+import { Body } from '@typography';
+import { RoundedButton, SmallSelect, TableHeader } from '@molecules';
+import { Header, ReceivingTableRow, Dialog, DatePicker, ReceivingWritableTable } from '@organisms';
 import { useDocumentTitle } from '@hooks';
+import { getReceivingsByYear, getYears, getClients, createReceiving, updateReceiving, deleteReceiving } from '@middleware';
+import { IClient, IReceiving } from '@types';
+import { getSpacedDecimal } from '@utils';
 import { IPageProps } from "../types";
-import { ReceivingsContainer } from './styles';
+import { ReceivingsContainer, InputsWrapper, CountersWrapper, CounterContainer } from './styles';
+import uniqueId from 'lodash.uniqueid';
 
 
 const Receivings: FunctionComponent<IPageProps> = ({ name }) => {
   useDocumentTitle(name);
+
+  const rowsState = useState<GridValidRowModel[]>([
+    {
+      id: uniqueId(),
+      weight: '',
+      price: '',
+      sum: ''
+    }
+  ]);
+
+  const [rows, setRows] = rowsState;
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogMode, setDialogMode] = useState<'create' | 'edit'>('create');
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
+  const [receivings, setReceivings] = useState<IReceiving[] | null>();
+  const [years, setYears] = useState<number[]>([]);
+  const [year, setYear] = useState<number | null>(null);
+  const [clients, setClients] = useState<IClient[]>([]);
+  const [client, setClient] = useState<string>('default');
+
+  const [date, setDate] = useState<Dayjs>(dayjs(Date.now()));
+
+  const [isTableCompleted, setIsTableCompleted] = useState(false);
+
+  const isFormCompleted = client !== 'default' && isTableCompleted;
+
+  const [editReceivingId, setEditReceivingId] = useState<string>('');
+  const [receivingToDelete, setReceivingToDelete] = useState<IReceiving | null>(null);
+
+  const getData = async () => {
+    if (year) {
+      getReceivingsByYear(year).then((res) => {
+        setReceivings(res.data);
+      });
+    }
+  };
+
+  const handleCreateReceivingButtonClick = () => {
+    setDialogMode('create');
+    setDialogOpen(true);
+  };
+
+  const handleDialogCancel = () => {
+    setDialogOpen(false);
+    setClient('default');
+    setRows([{
+      id: uniqueId(),
+      weight: '',
+      price: '',
+      sum: ''
+    }]);
+    setDate(dayjs(Date.now()));
+
+    if (dialogMode === 'edit') {
+      setEditReceivingId('');
+    }
+  };
+
+  const handleCreateReceivingConfirm = () => {
+    createReceiving({
+      client,
+      records: rows.map(({ weight, price }) => ({ weight, price })),
+      timestamp: date.toDate().getTime()
+    }).then((res) => {
+      const newReceiving = res.data;
+
+      if (new Date(newReceiving.timestamp).getFullYear() === year) {
+        setReceivings((prev) => [...prev, newReceiving].sort((a,b) => b.timestamp - a.timestamp));
+      }
+
+      handleDialogCancel();
+    });
+  };
+
+  const handleEditReceivingButtonClick = (id: string) => () => {
+    const receiving = receivings.find((r) => r._id === id);
+
+    if (receiving) {
+      setDialogMode('edit');
+      setEditReceivingId(id);
+      setRows(receiving.records.map(({ price, weight }) => ({
+        id: uniqueId(),
+        weight,
+        price,
+        sum: getSpacedDecimal(weight * price)
+      })));
+      setClient(receiving.client._id);
+      setDate(dayjs(receiving.timestamp));
+      setDialogOpen(true);
+    } else {
+      alert('Помилка! Щось не так з ідентифікатором.');
+    }
+  };
+
+  const handleEditReceivingConfirm = () => {
+    updateReceiving({
+      _id: editReceivingId,
+      client,
+      timestamp: date.toDate().getTime(),
+      records: rows.map(({ weight, price }) => ({ weight, price }))
+    }).then((res) => {
+      const updatedReceiving = res.data;
+
+      setReceivings((prev) => prev.filter((r) => r._id !== updatedReceiving._id));
+
+      if (new Date(updatedReceiving.timestamp).getFullYear() === year) {
+        setReceivings((prev) => [...prev, updatedReceiving].sort((a,b) => b.timestamp - a.timestamp));
+      }
+
+      handleDialogCancel();
+    })
+  };
+
+  const handleDeleteReceivingButtonClick = (id: string) => () => {
+    const receiving = receivings.find((r) => r._id === id);
+
+    if (receiving) {
+      setReceivingToDelete(receiving);
+      setDeleteDialogOpen(true);
+    } else {
+      alert('Помилка! Щось не так з ідентифікатором');
+    }
+
+  };
+
+  const handleDeleteReceivingConfirm = () => {
+    deleteReceiving(receivingToDelete._id).then(() => {
+      setReceivings((prev) => prev.filter((r) => r._id !== receivingToDelete._id));
+      setDeleteDialogOpen(false);
+
+      setTimeout(() => {
+        setReceivingToDelete(null);
+      }, 100);
+    });
+  }
+
+  const handleDeleteReceivingCancel = () => {
+    setDeleteDialogOpen(false);
+    setTimeout(() => {
+      setReceivingToDelete(null);
+    }, 100);
+  }
+
+  const handleTableChange = (isCompleted: boolean) => {
+    setIsTableCompleted(isCompleted);
+  };
+
+  useEffect(() => {
+    getYears().then((res) => {
+      setYears(res.data);
+    })
+
+    getClients().then((res) => {
+      setClients(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setYear(years[0] || null);
+  }, [years]);
+
+  useEffect(() => {
+    getData();
+  }, [year]);
 
   return (
     <>
       <Header
         title={name}
         controls={
-          <RoundedButton
-            text="Додати прийоми"
-            icon={<AddIcon />}
-            variant="contained"
-          />
+          <>
+            <SmallSelect
+              options={years.map((y) => ({
+                label: y,
+                value: y
+              }))}
+              option={year}
+              onChange={(event) => {
+                setYear(Number(event.target.value));
+              }}
+            />
+
+            <RoundedButton
+              text="Додати прийом"
+              icon={<AddIcon />}
+              variant="contained"
+              onClick={handleCreateReceivingButtonClick}
+            />
+          </>
         }
         tableHeader={ 
             <TableHeader width={600} headers={['Вага (кг)', 'Ціна (грн)', 'Сума (грн)', 'Всього (грн)']} />
@@ -31,25 +221,75 @@ const Receivings: FunctionComponent<IPageProps> = ({ name }) => {
       />  
       
       <ReceivingsContainer>
-        {new Array(30).fill(0).map((_, i) => (
-          <ReceivingTableRow
-            key={i}
-            timestamp={Date.now()}
-            client={{
-              _id: '0',
-              name: 'Чернявка Саня'
-            }}
-            rows={[
-              [30, 90, 1230],
-              [60, 20, 1231],
-              [50, 30, 3000],
-              [70, 10, 700],
-            ]}
-            allSum={400000}
-          />
-        ))}
-        
+          {receivings ? receivings?.map(({ _id, client, timestamp, records }) => (
+            <ReceivingTableRow
+              key={`${client._id}-${_id}`}
+              timestamp={timestamp}
+              client={client}
+              rows={records.map(({ weight, price }) => [
+                weight,
+                price,
+                weight * price
+              ])}
+              allSum={records.reduce((acc, curr) => acc + curr.price * curr.weight, 0)}
+              onEdit={handleEditReceivingButtonClick(_id)}
+              onDelete={handleDeleteReceivingButtonClick(_id)}
+            />
+          )) : <CircularProgress color="primary" />}
       </ReceivingsContainer>
+      <Dialog
+        title={`${dialogMode === 'create' ? 'Додати' : 'Редагувати'} прийом`}
+        open={dialogOpen}
+        disableConfirm={!isFormCompleted}
+        onConfirm={dialogMode === 'create' ? handleCreateReceivingConfirm : handleEditReceivingConfirm}
+        onCancel={handleDialogCancel}
+        preButtonsComponent={
+          <CountersWrapper>
+            <CounterContainer>
+              <AllWeightIcon />
+              <Body>Загальна вага:</Body>
+              <Body>{rows.reduce((acc, { weight }) => acc + (weight === '' ? 0 : weight), 0)} кг</Body>
+            </CounterContainer>
+            <CounterContainer>
+              <AllPriceIcon />
+              <Body>Загальна ціна:</Body>
+              <Body>{getSpacedDecimal(rows.reduce((acc, { weight, price }) => acc + (weight === '' || price === '' ? 0 : weight * price), 0))} грн</Body>
+            </CounterContainer>
+          </CountersWrapper>
+        }
+      >
+        <InputsWrapper>
+          <SmallSelect
+            defaultLabel="Оберіть клієнта"
+            options={clients.map((c) => ({
+              label: c.name,
+              value: c._id
+            }))}
+            option={client}
+            onChange={(event) => {
+              setClient(event.target.value);
+            }}
+          />
+          <DatePicker
+            value={date}
+            onChange={(newValue) => {
+              setDate(newValue);
+            }}
+          />
+        </InputsWrapper>
+        <ReceivingWritableTable
+          rowsState={rowsState}
+          onChange={handleTableChange}
+        />
+      </Dialog>
+      <Dialog
+        title={`Увага`}
+        open={deleteDialogOpen}
+        onConfirm={handleDeleteReceivingConfirm}
+        onCancel={handleDeleteReceivingCancel}
+      >
+        <Body>Ви впевнені, що хочете видалити прийом клієнта <b>{ receivingToDelete?.client.name }</b> датований <b>{ dayjs(receivingToDelete?.timestamp).format('DD.MM.YYYY') }</b>?</Body>
+      </Dialog>
     </>
   );
 };
--- src/pages/receivings/styles.ts
+++ src/pages/receivings/styles.ts
@@ -7,3 +7,21 @@ export const ReceivingsContainer = styled.div`
   padding-bottom: 24px;
   flex: 1;
 `;
+
+export const InputsWrapper = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+`;
+
+export const CountersWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+`;
+
+export const CounterContainer = styled.div`
+  display: flex;
+  column-gap: 12px;
+`;
