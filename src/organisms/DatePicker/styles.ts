0fc3a2a253779e76dcd8cb6b0dfffb20df4905e1
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-09-10 15:43:46 +0200

    Date Picker: fix style for calendar icon

--- src/organisms/DatePicker/styles.ts
+++ src/organisms/DatePicker/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import { inputAdornmentClasses } from '@mui/material';
 import { DatePicker } from "@mui/x-date-pickers";
 
 export const StyledDatePicker = styled(DatePicker)`
-
+  .${inputAdornmentClasses.root} > button {
+    margin-right: 4px;
+  }
 `;
